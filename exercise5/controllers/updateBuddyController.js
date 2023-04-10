d6f444b7fae380c564817b706e2184d7caffe868
ksakthivel1211 <125632352+ksakthivel1211@users.noreply.github.com>
2023-04-10 10:32:27 +0530

    Update updateBuddyController.js

--- exercise5/controllers/updateBuddyController.js
+++ exercise5/controllers/updateBuddyController.js
@@ -1,9 +1,9 @@
 let fs = require('fs');
 const logger = require('../logger');
-const updateBuddyinfo = require('../services/updateService')
+const {updateBuddyinfo} = require('../services/updateService')
 
 const updateBuddy = async (req,res) => {
-    const responseData = await addBuddyDetails(req, res);
+    const responseData = await updateBuddyinfo(req, res);
     
     res.status(responseData.code).send(responseData.message);
 }
