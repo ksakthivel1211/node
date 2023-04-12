2f4558811e2f624c9ef6d4601b96492e42509806
ksakthivel1211 <125632352+ksakthivel1211@users.noreply.github.com>
2023-04-12 09:59:34 +0530

    commit

--- exercise5/controllers/updateBuddyController.js
+++ exercise5/controllers/updateBuddyController.js
@@ -1,11 +1,11 @@
 let fs = require('fs');
 const logger = require('../logger');
 const {updateBuddyinfo} = require('../services/updateService')
-
+const response = require('../response');
 const updateBuddy = async (req,res) => {
     logger.info(`START :: Update Buddy controller`);
-    if(req.body.employeeID !== undefined && req.body.employeeID !== null && Number.isInteger(eq.body.employeeID)  &&  req.body.realName !== undefined && req.body.realName !== null &&
-    req.body.nickName !== undefined && req.body.nickName !== null && (req.body.dob instanceof Date) && req.body.dob !== null &&
+    if(req.body.employeeId !== undefined && req.body.employeeId !== null &&  req.body.realName !== undefined && req.body.realName !== null &&
+    req.body.nickName !== undefined && req.body.nickName !== null && req.body.dob !== null &&
     req.body.hobbies !== undefined && req.body.hobbies !== null)
     {
     const responseData = await updateBuddyinfo(req, res);
